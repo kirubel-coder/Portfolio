ba3d30d14f77dbd8cd5b1e283a9dd6105fa22920
kirubel-coder <kirubeladdis364@gmail.com>
2025-04-05 22:52:42 -0700

    Fixed entry point to main.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import '../css/Home.css';
 import Survice from '../components/Survice.jsx';
 import Project from '../components/Project.jsx';
 import Contact from '../components/Contact.jsx';
-import upworkPfpCropped3 from "../assets/upworkPfpCropped3.jpg";
+import upworkPfpCropped2 from "../assets/upworkPfpCropped2.jpg";
 import ppforAbout from "../assets/ppforAbout.jpg";
 import newFullstack from "../assets/newFullstack.png";
 import frontend_iconnew from "../assets/frontend_iconnew.png";
@@ -20,7 +20,7 @@ function Home() {
     <div>
       <Element name="home" className="hero-section">
         <div className="hero-left">
-          <img className="top-profile" src={upworkPfpCropped3} alt="profile picture" />
+          <img className="top-profile" src={upworkPfpCropped2} alt="profile picture" />
         </div>
         <div className="hero-right">
           <h1>Hey, I'm Kirubel Addis</h1>
